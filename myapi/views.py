49db2aa1116553744f3dda0df9b90032e6a2ba1f
Kim-dong-kyun <kdk942@gmail.com>
2026-05-20 01:37:58 +0900

    feat: handled_by

--- myapi/views.py
+++ myapi/views.py
@@ -4,12 +4,9 @@ from .models import Product
 
 def product_list(request):
 
-    products = Product.objects.all()
     container_id = socket.gethostname()
     data = {
-        {   "handled_by" : container_id,
-            "name": product.name,"price" : product.price}
-        for product in products
+        "handled_by": container_id
     }
 
     return JsonResponse(data)
